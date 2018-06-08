a3ef56cb359048fc7f83180979a3617496a07a3a
Patrick Griffith <Patrick.Griffith@NSERC-CRSNG.GC.CA>
2018-06-08 11:02:02 -0400

    Adding the demonstration of EasyReprov9.0.0.1 used for the ARB in May 2018.

---
+++ Automated UI/Automated UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Automated UI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Automated UI")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e550e68-604e-4c3f-84b3-25828403365f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
